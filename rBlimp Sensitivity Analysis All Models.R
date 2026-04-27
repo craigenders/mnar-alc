1e45836ba0ba12bb8660bc8235e10484a46a862c
craigenders <cenders@ucla.edu>
2026-04-26 17:09:53 -0700

    Updates

--- rBlimp Sensitivity Analysis All Models.R
+++ rBlimp Sensitivity Analysis All Models.R
@@ -9,37 +9,27 @@ library(ggplot2)
 library(patchwork)
 library(rblimp)
 set_blimp('/applications/blimp/blimp-nightly')
-# remotes::update_packages('rblimp')
 
 #------------------------------------------------------------------------------#
 # READ DATA ----
 #------------------------------------------------------------------------------#
 
 # github url for raw data
-filepath1 <- 'https://raw.githubusercontent.com/craigenders/mnar-mlm/main/growth-dropout.csv'
+filepath <- 'https://raw.githubusercontent.com/craigenders/mnar-alc/main/mnar.csv'
 
 # create data frame from github data
-growth <- read.csv(filepath1, stringsAsFactors = T)
-growth <- growth[!is.na(growth$m),]
-
+mnar <- read.csv(filepath, stringsAsFactors = T)
 
 # plotting functions
 source('https://raw.githubusercontent.com/blimp-stats/blimp-book/main/misc/functions.R')
 source('https://raw.githubusercontent.com/craigenders/mnar-mlm/main/mnar-plotting.R')
 
-# compute vars
-growth$med <- growth$group
-growth$month <- growth$time
-growth$dpdd <- growth$y - min(growth$ycom, na.rm = TRUE)
-mnar <- growth[growth$id < 166,c('id','med','month','dpdd')]
-write.csv(mnar, file = '~/desktop/mnar.csv', row.names = F)
-
 #------------------------------------------------------------------------------#
-# CMAR MODELS ----
+# CMAR MODEL ----
 #------------------------------------------------------------------------------#
 
-# cmar analysis model 1 ----
-cmar_mod1 <- rblimp(
+# MODEL 1: CMAR ----
+model1_cmar <- rblimp(
   data = mnar,
   ordinal = 'med', # define med as categorical
   clusterid = 'id', # person id variable
@@ -66,22 +56,60 @@ cmar_mod1 <- rblimp(
 )
 
 # print output
-output(cmar_mod1)
+output(model1_cmar)
 
 # plot model-predicted means
-bivariate_plot(dpdd.predicted ~ month | med, model = cmar_mod1, discrete_x = 'month', points = F, ci = F) + ylim(0,15)
+bivariate_plot(dpdd.predicted ~ month | med, 
+               model = model1_cmar, 
+               discrete_x = 'month', 
+               points = F, ci = F) + ylim(0,15)
 
 #------------------------------------------------------------------------------#
 # TIME-RELATED DROPOUT ----
 #------------------------------------------------------------------------------#
 
-# dummy codes with group by time interaction
-dropout_time <- rblimp(
+# DROPOUT PROBS: Linear Time ----
+time_linear <- rblimp(
   data = mnar,
   ordinal = 'med', # define med as categorical
   clusterid = 'id', # person id variable 
   timeid = 'month', # time identifier for dropout and lagged effects 
-  dropout = 'dropout = dpdd', # create dropout indicator
+  dropout = 'dropout = dpdd (monotone)', # create dropout indicator
+  model = 'dropout ~ intercept@-3 month month*med | intercept@0',
+  seed = 90291, # random number seed
+  burn = 20000, # burn-in iterations
+  iter = 20000, # iterations for analysis summaries
+  nimps = 20 # save imputations for graphing trajectories
+)
+
+# print output
+output(time_linear)
+
+# DROPOUT PROBS: Quadratic Time ----
+time_quadratic <- rblimp(
+  data = mnar,
+  ordinal = 'med', # define med as categorical
+  clusterid = 'id', # person id variable 
+  timeid = 'month', # time identifier for dropout and lagged effects 
+  dropout = 'dropout = dpdd (monotone)', # create dropout indicator
+  model = 'dropout ~ intercept@-3 month month^2 month*med month^2*med | intercept@0',
+  seed = 90291, # random number seed
+  burn = 20000, # burn-in iterations
+  iter = 20000, # iterations for analysis summaries
+  nimps = 20 # save imputations for graphing trajectories
+)
+
+# print output
+output(time_quadratic)
+
+
+# DROPOUT PROBS: Dummy-Coded Time ----
+time_dummy <- rblimp(
+  data = mnar,
+  ordinal = 'med', # define med as categorical
+  clusterid = 'id', # person id variable 
+  timeid = 'month', # time identifier for dropout and lagged effects 
+  dropout = 'dropout = dpdd (monotone)', # create dropout indicator
   # (month == 1) (month == 2) etc. are dummy codes for time
   model = 'dropout ~ intercept@-3 (month==1) (month==2) (month==3) (month==4)
       (month==1)*med (month==2)*med (month==3)*med (month==4)*med | intercept@0',
@@ -92,26 +120,70 @@ dropout_time <- rblimp(
 )
 
 # print output
-output(dropout_time)
+output(time_dummy)
 
-# observed dropout rates
-aggregate(dropout ~ month + med, data = dropout_time@average_imp, FUN = mean)
+#------------------------------------------------------------------------------#
+# PLOT MISSINGNESS PROBABILITIES ----
+#------------------------------------------------------------------------------#
 
-# model-predicted dropout rates
-bivariate_plot(dropout.1.probability ~ month | med, model = dropout_time, discrete_x = 'month', points = F, ci = F) + ylim(0,.40)
+ymin <- 0
+ymax <- .4
+
+# plot observed probabilities
+plot_pmiss_obs <- plot_means(dropout ~ month | med, 
+    model = time_dummy,
+    ylab = "Probability",
+    title = "Observed Probabilities",
+    group_labels = c("0" = "0", "1" = "1")) + ylim(ymin,ymax) +
+    theme(legend.position = "top",legend.justification = "center") +
+    scale_linetype_manual(values = c("dashed", "solid")) +
+    geom_line(linewidth = .25)
+
+# plot predicted probabilities from linear model
+plot_pmiss_linear <- plot_means(dropout.1.probability ~ month | med, 
+    model = time_linear,
+    ylab = "Probability",
+    title = "Linear Time",
+    group_labels = c("0" = "0", "1" = "1")) + ylim(ymin,ymax) +
+    theme(legend.position = "top",legend.justification = "center") +
+    scale_linetype_manual(values = c("dashed", "solid")) +
+    geom_line(linewidth = .25)
+
+# plot predicted probabilities from quadratic model
+plot_pmiss_quadratic <- plot_means(dropout.1.probability ~ month | med, 
+    model = time_quadratic,
+    ylab = "Probability",
+    title = "Quadratic Time",
+    group_labels = c("0" = "0", "1" = "1")) + ylim(ymin,ymax) +
+    theme(legend.position = "top",legend.justification = "center") +
+    scale_linetype_manual(values = c("dashed", "solid")) +
+    geom_line(linewidth = .25)
+
+# plot predicted probabilities from dummy-coded model
+plot_pmiss_dummy <- plot_means(dropout.1.probability ~ month | med, 
+    model = time_dummy,
+    ylab = "Probability",
+    title = "Dummy-Coded Time",
+    group_labels = c("0" = "0", "1" = "1")) + ylim(ymin,ymax) +
+    theme(legend.position = "top",legend.justification = "center") +
+    scale_linetype_manual(values = c("dashed", "solid")) +
+    geom_line(linewidth = .25)
+
+# combine plots with patchwork
+plot_pmiss <- (plot_pmiss_obs | plot_pmiss_linear) / (plot_pmiss_quadratic | plot_pmiss_dummy)
+plot_pmiss
 
 #------------------------------------------------------------------------------#
 # SHARED PARAMETER MODELS ----
 #------------------------------------------------------------------------------#
 
-# shared parameter model 3: intercepts and slopes predicting dropout ----
-sp_mod3 <- rblimp(
+# MODEL 3: Shared Parameter Model With Random Effects Predicting Dropout ----
+model3_sharedparam <- rblimp(
   data = mnar,
   ordinal = 'med', # define med as categorical
   clusterid = 'id', # person id variable
   timeid = 'month', # time identifier for dropout and lagged effects 
-  dropout = 'dropout = dpdd', # create dropout indicator
-  # transform = 'preknot = ifelse(month == 0, 0, 1)',
+  dropout = 'dropout = dpdd (monotone)', # create dropout indicator
   latent = 'id = b0i b1i', # define random effects as latent variables
   fixed = 'month med', # complete predictors
   model = '
@@ -140,19 +212,21 @@ sp_mod3 <- rblimp(
 )
 
 # print output
-output(sp_mod3)
+output(model3_sharedparam)
 
 # plot model-predicted means
-bivariate_plot(dpdd.predicted ~ month | med, model = sp_mod3, discrete_x = 'month', points = F, ci = F) + ylim(0,15)
+bivariate_plot(dpdd.predicted ~ month | med, 
+               model = model3_sharedparam, 
+               discrete_x = 'month', 
+               points = F, ci = F) + ylim(0,15)
 
-# shared parameter model 3: intercepts and slopes and their squares predicting dropout ----
-sp_mod5 <- rblimp(
+# MODEL 5: Shared Parameter Model With Random Effects and Their Squares Predicting Dropout ----
+model5_sharedparam <- rblimp(
   data = mnar,
   ordinal = 'med', # define med as categorical
   clusterid = 'id', # person id variable
   timeid = 'month', # time identifier for dropout and lagged effects 
-  dropout = 'dropout = dpdd', # create dropout indicator
-  # transform = 'preknot = ifelse(month == 0, 0, 1)',
+  dropout = 'dropout = dpdd (monotone)', # create dropout indicator
   latent = 'id = b0i b1i', # define random effects as latent variables
   fixed = 'month med', # complete predictors
   model = '
@@ -181,23 +255,25 @@ sp_mod5 <- rblimp(
 )
 
 # print output
-output(sp_mod5)
+output(model5_sharedparam)
 
 # plot model-predicted means
-bivariate_plot(dpdd.predicted ~ month | med, model = sp_mod5, discrete_x = 'month', points = F, ci = F) + ylim(0,15)
+bivariate_plot(dpdd.predicted ~ month | med, 
+               model = model5_sharedparam, 
+               discrete_x = 'month', 
+               points = F, ci = F) + ylim(0,15)
 
 #------------------------------------------------------------------------------#
 # SELECTION MODELS MODELS ----
 #------------------------------------------------------------------------------#
 
-# diggle-kenward selection model 6: current (missing) drinking predicting dropout ----
-dk_mod6 <- rblimp(
+# MODEL 6: Selection Model With Outcome Predicting Dropout ----
+model6_selection <- rblimp(
   data = mnar,
   ordinal = 'med', # define med as categorical
   clusterid = 'id', # person id variable
   timeid = 'month', # time identifier for dropout and lagged effects 
-  dropout = 'dropout = dpdd', # create dropout indicator
-  # transform = 'preknot = ifelse(month == 0, 0, 1)',
+  dropout = 'dropout = dpdd (monotone)', # create dropout indicator
   latent = 'id = b0i b1i', # define random effects as latent variables
   fixed = 'month med', # complete predictors
   model = '
@@ -226,18 +302,21 @@ dk_mod6 <- rblimp(
 )
 
 # print output
-output(dk_mod6)
+output(model6_selection)
 
 # plot model-predicted means
-bivariate_plot(dpdd.predicted ~ month | med, model = dk_mod6, discrete_x = 'month', points = F, ci = F) + ylim(0,15)
+bivariate_plot(dpdd.predicted ~ month | med, 
+               model = model6_selection, 
+               discrete_x = 'month', 
+               points = F, ci = F) + ylim(0,15)
 
-# diggle-kenward selection model 7: current (missing) drinking and its square predicting dropout ----
-dk_mod7 <- rblimp(
+# MODEL 7: Selection Model With Outcome and its Square Predicting Dropout ----
+model7_selection <- rblimp(
   data = mnar,
   ordinal = 'med', # define med as categorical
   clusterid = 'id', # person id variable
   timeid = 'month', # time identifier for dropout and lagged effects 
-  dropout = 'dropout = dpdd', # create dropout indicator
+  dropout = 'dropout = dpdd (monotone)', # create dropout indicator
   latent = 'id = b0i b1i', # define random effects as latent variables
   fixed = 'month med', # complete predictors
   model = '
@@ -266,19 +345,21 @@ dk_mod7 <- rblimp(
 )
 
 # print output
-output(dk_mod7)
+output(model7_selection)
 
 # plot model-predicted means
-bivariate_plot(dpdd.predicted ~ month | med, model = dk_mod7, discrete_x = 'month', points = F, ci = F) + ylim(0,15)
+bivariate_plot(dpdd.predicted ~ month | med, 
+               model = model7_selection, 
+               discrete_x = 'month', 
+               points = F, ci = F) + ylim(0,15)
 
-# diggle-kenward selection model 8: current (missing) and prior (observed) drinking predicting dropout ----
-dk_mod8 <- rblimp(
+# MODEL 8: Selection Model With Outcome and its Lag Predicting Dropout ----
+model8_selection <- rblimp(
   data = mnar,
-  # transform = 'preknot = ifelse(month == 0, 0, 1)',
   ordinal = 'med', # define med as categorical
   clusterid = 'id', # person id variable
   timeid = 'month', # time identifier for dropout and lagged effects 
-  dropout = 'dropout = dpdd', # create dropout indicator
+  dropout = 'dropout = dpdd (monotone)', # create dropout indicator
   latent = 'id = b0i b1i', # define random effects as latent variables
   fixed = 'month med', # complete predictors
   model = '
@@ -307,10 +388,130 @@ dk_mod8 <- rblimp(
 )
 
 # print output
-output(dk_mod8)
+output(model8_selection)
 
 # plot model-predicted means
-bivariate_plot(dpdd.predicted ~ month | med, model = dk_mod8, discrete_x = 'month', points = F, ci = F) + ylim(0,15)
+bivariate_plot(dpdd.predicted ~ month | med, 
+               model = model8_selection, 
+               discrete_x = 'month', 
+               points = F, ci = F) + ylim(0,15)
+
+#------------------------------------------------------------------------------#
+# EXTRACT ESTIMATES ----
+#------------------------------------------------------------------------------#
+
+# function to extract key estimates
+extract_growth_params <- function(object, method) {
+  
+  tab <- object@estimates
+  
+  rows <- c(
+    "b0i ~ Intercept",
+    "b1i ~ Intercept",
+    "b0i ~ med",
+    "b1i ~ med",
+    "b0i residual variance",
+    "b1i residual variance",
+    "Cor( b0i, b1i )",
+    "dpdd residual variance",
+    "Parameter: d_month0",
+    "Parameter: d_month1",
+    "Parameter: d_month2",
+    "Parameter: d_month3",
+    "Parameter: d_month4",
+    "dropout R2: Coefficients"
+  )
+  
+  res <- do.call(rbind, lapply(rows, function(r) {
+    if (r %in% rownames(tab)) {
+      round(tab[r, c("Estimate", "StdDev"), drop = FALSE], 2)
+    } else {
+      data.frame(Estimate = NA_real_, StdDev = NA_real_, row.names = r)
+    }
+  }))
+  
+  rownames(res) <- c(
+    "Icept (M = 0)",
+    "Slope (M = 0)",
+    "Icept Diff.",
+    "Slope Diff.",
+    "Var(Icept)",
+    "Var(Slope)",
+    "Cor(Icept, Slope)",
+    "Var(Residual)",
+    "Std. Diff. T0",
+    "Std. Diff. T1",
+    "Std. Diff. T2",
+    "Std. Diff. T3",
+    "Std. Diff. T4",
+    "Pseudo-Rsq"
+  )
+  
+  colnames(res) <- c(
+    paste0("Est_", method),
+    paste0("SD_", method)
+  )
+  
+  res
+}
+
+# main summary table ----
+table_summary <- cbind(
+  extract_growth_params(model1, "M1"),
+  extract_growth_params(model2, "M2"),
+  extract_growth_params(model3, "M3"),
+  extract_growth_params(model4, "M4"),
+  extract_growth_params(model5, "M5"),
+  extract_growth_params(model6, "M6"),
+  extract_growth_params(model7, "M7"),
+  extract_growth_params(model8, "M8")
+)
+table_summary
+
+# diagnostics table ----
+
+# function to extract convergence diagnostics
+extract_convergence <- function(object, method) {
+  
+  neff <- object@estimates[, "N_Eff"]
+  neff <- neff[is.finite(neff)]
+  
+  psr_row <- as.numeric(object@psr[20, ])
+  psr_row <- psr_row[is.finite(psr_row)]
+  
+  data.frame(
+    Min_Neff = round(min(neff),    3),
+    Max_Neff = round(max(neff),    3),
+    Min_PSR  = round(min(psr_row), 3),
+    Max_PSR  = round(max(psr_row), 3),
+    row.names = method
+  )
+}
+
+# build table
+table_diag <- rbind(
+  extract_convergence(model1, "Model 1"),
+  extract_convergence(model2, "Model 2"),
+  extract_convergence(model3, "Model 3"),
+  extract_convergence(model4, "Model 4"),
+  extract_convergence(model5, "Model 5"),
+  extract_convergence(model6, "Model 6"),
+  extract_convergence(model7, "Model 7"),
+  extract_convergence(model8, "Model 8")
+)
+
+# add number of iterations
+table_diag$Iterations <- c(
+  nrow(model1@iterations),
+  nrow(model2@iterations),
+  nrow(model3@iterations),
+  nrow(model4@iterations),
+  nrow(model5@iterations),
+  nrow(model6@iterations),
+  nrow(model7@iterations),
+  nrow(model8@iterations)
+)
+table_diag
 
 
 
